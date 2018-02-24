885239db55aa4ca123f3860e2e464c4edb08aa24
haoxingling <hxl4321>
2018-02-24 17:16:07 +0800

    添加加密算法

--- app/src/main/java/fuzik/com/myapplication/MainActivity.java
+++ app/src/main/java/fuzik/com/myapplication/MainActivity.java
@@ -3,12 +3,83 @@ package fuzik.com.myapplication;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.mysirui.vehicle.SRBleSDK;
+import com.sprout.frame.baseframe.utils.coder.AES;
+
+import org.bouncycastle.util.encoders.Hex;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends UIBaseActivity {
+
+
+    SRBleSDK bleSDK;
+    AES aes;
+    String key;
+    String content;
 
     @SuppressLint("MissingSuperCall")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState,R.layout.activity_main);
+        super.onCreate(savedInstanceState, R.layout.activity_main);
+        aes = new AES();
+        //   加解密 密钥
+        key = "LD-FZ-1707200004";
+        content = "1234567890123456";
+//        bleSDK
+//                .onConnect(new Action0() {
+//                    @Override
+//                    public void call() {
+//
+//                    }
+//                })
+//                .onLogin(new Action0() {
+//                    @Override
+//                    public void call() {
+//
+//                    }
+//                })
+//                .onDisconn(new Action0() {
+//                    @Override
+//                    public void call() {
+//
+//                    }
+//                })
+//                .start()
+        Observable.interval(3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .subscribe(new Subscriber<Long>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        // 加密字符串
+                        Log.d("--------","加密前的：" + content);
+                        Log.d("--------","加密密钥：" + key);
+                        // 加密方法
+                        byte[] enc = aes.encrypt(content.getBytes(), key.getBytes());
+                        Log.d("--------","加密后的内容：" + new String(Hex.encode(enc)));
+                        aes.iv1 = new String(Hex.encode(enc));
+//                        // 解密方法
+//                        byte[] dec = aes.decrypt(enc, key.getBytes());
+//                        System.out.println("解密后的内容：" + new String(dec));
+
+                    }
+                });
     }
 }
---
+++ baseframe/src/main/java/com/sprout/frame/baseframe/utils/coder/AES.java
@@ -0,0 +1,104 @@
+package com.sprout.frame.baseframe.utils.coder;
+
+
+import android.util.Log;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AES {
+    // 算法名称
+    final String KEY_ALGORITHM = "AES";
+    // 加解密算法/模式/填充方式
+    final String algorithmStr = "AES/CBC/PKCS7Padding";
+    //
+    private Key key;
+    private Cipher cipher;
+    boolean isInited = false;
+    public String iv1 = "LD-FZ-1707200004";
+    public String iv2 = "";
+    byte[] iv = { 0x30, 0x31, 0x30, 0x32, 0x30, 0x33, 0x30, 0x34, 0x30, 0x35, 0x30, 0x36, 0x30, 0x37, 0x30, 0x38 };
+    public void init(byte[] keyBytes) {
+
+        // 如果密钥不足16位，那么就补足.  这个if 中的内容很重要
+        int base = 16;
+        if (keyBytes.length % base != 0) {
+            int groups = keyBytes.length / base + (keyBytes.length % base != 0 ? 1 : 0);
+            byte[] temp = new byte[groups * base];
+            Arrays.fill(temp, (byte) 0x07);
+            System.arraycopy(keyBytes, 0, temp, 0, keyBytes.length);
+            keyBytes = temp;
+        }
+        // 初始化
+        Security.addProvider(new BouncyCastleProvider());
+        // 转化成JAVA的密钥格式
+        key = new SecretKeySpec(keyBytes, KEY_ALGORITHM);
+        try {
+            // 初始化cipher
+            cipher = Cipher.getInstance(algorithmStr, "BC");
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 加密方法
+     *
+     * @param content
+     *            要加密的字符串
+     * @param keyBytes
+     *            加密密钥
+     * @return
+     */
+    public byte[] encrypt(byte[] content, byte[] keyBytes) {
+        byte[] encryptedText = null;
+        init(keyBytes);
+        Log.d("--------","IV：" + iv1);
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv1.getBytes()));
+            encryptedText = cipher.doFinal(content);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return encryptedText;
+    }
+    /**
+     * 解密方法
+     *
+     * @param encryptedData
+     *            要解密的字符串
+     * @param keyBytes
+     *            解密密钥
+     * @return
+     */
+    public byte[] decrypt(byte[] encryptedData, byte[] keyBytes) {
+        byte[] encryptedText = null;
+        init(keyBytes);
+        System.out.println("IV：" + iv2);
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv2.getBytes()));
+            encryptedText = cipher.doFinal(encryptedData);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return encryptedText;
+    }
+}
