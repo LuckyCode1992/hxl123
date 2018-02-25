68f34959ffcc99ffb8ff30be3628b3cb02346a53
haoxingling <hxl4321>
2018-02-25 09:35:43 +0800

    更新gradle版本，解决无法显示设计图问题

--- app/src/main/java/fuzik/com/myapplication/MainActivity.java
+++ app/src/main/java/fuzik/com/myapplication/MainActivity.java
@@ -4,6 +4,10 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.mysirui.vehicle.SRBleSDK;
 import com.sprout.frame.baseframe.utils.coder.AES;
@@ -12,6 +16,8 @@ import org.bouncycastle.util.encoders.Hex;
 
 import java.util.concurrent.TimeUnit;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import rx.Observable;
 import rx.Subscriber;
 import rx.schedulers.Schedulers;
@@ -23,6 +29,18 @@ public class MainActivity extends UIBaseActivity {
     AES aes;
     String key;
     String content;
+    @BindView(R.id.tv_ble)
+    TextView tvBle;
+    @BindView(R.id.btn_connect)
+    Button btnConnect;
+    @BindView(R.id.et_jiami_qian)
+    EditText etJiamiQian;
+    @BindView(R.id.tv_jiami_hou)
+    TextView tvJiamiHou;
+    @BindView(R.id.btn_send)
+    Button btnSend;
+    @BindView(R.id.tv_jiesou)
+    TextView tvJiesou;
 
     @SuppressLint("MissingSuperCall")
     @Override
@@ -69,11 +87,11 @@ public class MainActivity extends UIBaseActivity {
                     @Override
                     public void onNext(Long aLong) {
                         // 加密字符串
-                        Log.d("--------","加密前的：" + content);
-                        Log.d("--------","加密密钥：" + key);
+                        Log.d("--------", "加密前的：" + content);
+                        Log.d("--------", "加密密钥：" + key);
                         // 加密方法
                         byte[] enc = aes.encrypt(content.getBytes(), key.getBytes());
-                        Log.d("--------","加密后的内容：" + new String(Hex.encode(enc)));
+                        Log.d("--------", "加密后的内容：" + new String(Hex.encode(enc)));
                         aes.iv1 = new String(Hex.encode(enc));
 //                        // 解密方法
 //                        byte[] dec = aes.decrypt(enc, key.getBytes());
@@ -81,5 +99,15 @@ public class MainActivity extends UIBaseActivity {
 
                     }
                 });
+    }
+
+    @OnClick({R.id.btn_connect, R.id.btn_send})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_connect:
+                break;
+            case R.id.btn_send:
+                break;
+        }
     }
 }
